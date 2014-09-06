60c48346117efc3dac543bbd15769b77fc76eba2
zz <zz.at.field@gmail.com>
2014-09-06 19:26:39 +0800

    remove useless comment

--- mainapp/lib/choose.py
+++ mainapp/lib/choose.py
@@ -13,7 +13,7 @@ EXCLUDE = (2, 4, 5, 8, 10, 12, 13)
 #humanread(1, 3, 6, 7, 9, 11)
 STARTAT = (0, 2, 5, 6, 8, 10)
 WEEK = (1, 2, 3, 4, 5, 6, 7)
-supportkey = ('name', 'time', 'status')  #记得在course_table_update 中也修改
+supportkey = ('name', 'time', 'status')  # 记得在course_table_update 中也修改
 #SEASON = ('春', '夏', '秋', '冬')
 SEASON = (0, 1, 2, 3)
 
@@ -21,17 +21,8 @@ SEASON = (0, 1, 2, 3)
 
 
 
-
-
 class Day:
     """
-    data struct:
-        data[WEEK][STARTAT][supportkey]
-        for example,
-        data[1][2]['name'],
-        name of the Second lesson on Monday
-
-
 
     data struct:
         data[season][WEEK][STARTAT][supportkey]
@@ -109,17 +100,6 @@ class Day:
                                                     }
 
 
-"""
-class Season:
-
-
-    def __init__(self, season, week=WEEK):
-        self.season = season
-        self.week = week
-
-    def user_fullcourse(self):
-
-"""
 
 
 class Table:
@@ -131,9 +111,6 @@ class Table:
         self.season = SEASON
     def user_fullcourse(self):
 
-        #for whichday in self.week:
-        #    day = Day(whichday, self.table)
-        #    day.full()
         for whichseason in self.season:
             for whichday in self.week:
                 day = Day(whichseason, whichday, self.table)
